80e9a7a7421ad4b7b2164153e3b6f4f3f36f50bb
exhibit19 <exhibit4real@gmail.com>
2024-01-25 20:25:05 +0100

    created, the packages and respective classes

---
+++ src/test/java/com/bitscoder/Authentication_System/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitscoder.Authentication_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
